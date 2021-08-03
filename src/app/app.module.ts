ef99beb196e631e3d80f83d5a23783fb5002205c
Ahmet Akyapı <ahmetakyapii@gmail.com>
2021-08-03 14:09:58 +0300

    Create login, productList, shoppingCart Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login/login.component';
+import { ProductListComponent } from './productList/product-list/product-list.component';
+import { ShoppingCartComponent } from './shoppingCart/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProductListComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
