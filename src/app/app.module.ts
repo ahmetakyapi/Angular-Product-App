cfc934cedef9a7f5329059463127672d8aea3de4
Ahmet Akyapı <ahmetakyapii@gmail.com>
2021-08-04 10:58:38 +0300

    Update App Module and main.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { BasketComponent } from './basket/basket.component';
-import { ProductComponent } from './product/product.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { AddProductComponent } from './add-product/add-product.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { LoginComponent } from './login/login.component'
+import { BasketComponent } from './basket/basket.component'
+import { ProductComponent } from './product/product.component'
+import { NavbarComponent } from './navbar/navbar.component'
+import { AddProductComponent } from './add-product/add-product.component'
 
 @NgModule({
   declarations: [
@@ -18,11 +18,8 @@ import { AddProductComponent } from './add-product/add-product.component';
     NavbarComponent,
     AddProductComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
